28e2a5612fbf9d257cd4702b09b603e261563860
randrei12 <rantaandrei12@gmail.com>
2023-05-04 12:14:42 +0300

    #

--- src/components/DroppedElement.tsx
+++ src/components/DroppedElement.tsx
@@ -4,11 +4,26 @@ import { IF_SerializedElement } from "../interfaces/DroppedElement";
 export default function DroppedElement({ target } : { target: IF_SerializedElement }) {
     const Tag = config[target.type].html as keyof JSX.IntrinsicElements;
 
+    let content: string | number | false = false;
+    let styles: { [key: string]: string } = {};
+
+    target.styles.forEach(style => {
+        switch (style.type) {
+            case 'content':
+                content = style.value;
+                break;
+            case 'style':
+                styles[style.name] = `${style.value}${style.unit}`;
+                break;
+        }
+    })
+
     return (
-        <Tag>
+        <Tag style={styles}>
             { target.children.map(child => (
                 <DroppedElement key={child.id} target={child} />
             )) }
+            { target.children.length === 0 && content }
         </Tag>
     );
     
